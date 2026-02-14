1d8579c7b509b97b7a9c8460cc8670041b7c30a7
Arnab Datta <dattaarnab2013@gmail.com>
2026-02-14 23:57:39 +0530

    Pagination Components create

---
+++ src/components/BackendPagination.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BackendPagination() {
+  return <div>BackendPagination</div>;
+}
+
+export default BackendPagination;
---
+++ src/components/FrontendPagination.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FrontendPagination() {
+  return <div>FrontendPagination</div>;
+}
+
+export default FrontendPagination;
